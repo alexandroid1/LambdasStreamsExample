5d0d176cbb42de17a2c6c5a4adc8d56f93b7b246
alexandroid1 <alex123456>
2018-01-15 20:12:31 +0200

    collect(toList())

--- src/com/globallogic/StreamsExample1.java
+++ src/com/globallogic/StreamsExample1.java
@@ -1,8 +1,13 @@
 package com.globallogic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.lang.Character.isDigit;
+import static java.util.stream.Collectors.toList;
 
 public class StreamsExample1 {
 
@@ -25,8 +30,26 @@ public class StreamsExample1 {
         long count = people.parallelStream()
                 .filter(p -> p.getLastName().startsWith("D"))
                 .count();
-
         System.out.println(count);
+
+        //---  collect(toList()) ---
+
+        // example 1
+        List<String> collected = Stream.of("a", "b", "c")
+                .collect(toList());
+        System.out.println(collected); // "a", "b", "c"
+
+        // example 2
+        List<String> collected1 = Stream.of("a", "b", "hello")
+                .map(string -> string.toUpperCase())
+                .collect(toList());
+        System.out.println(collected1); // "A", "B", "HELLO"
+
+        // example 3
+        List<String> beginningWithNumbers = Stream.of("a", "1abc", "abc1")
+                .filter(value -> isDigit(value.charAt(0)))
+                .collect(toList());
+        System.out.println(beginningWithNumbers); // 1abc
 
     }
 }
